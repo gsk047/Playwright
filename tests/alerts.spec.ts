2e0fa56f39de3688a875c156d5863615e04320dc
gsk047 <gowthamsk047@gmail.com>
2026-03-14 16:49:07 +0530

    Alerts

---
+++ tests/alerts.spec.ts
@@ -0,0 +1,14 @@
+import {expect, test} from "@playwright/test";
+
+test('Simple Alert', async ({page}) => {
+    await page.goto('https://the-internet.herokuapp.com/javascript_alerts');
+    page.on('dialog', async (alert) => {
+        const alertMessage = alert.message();
+        expect(alertMessage).toBe('I am a JS Alert');
+        await alert.accept();
+    })
+
+    await page.locator('button[onclick="jsAlert()"]').click()
+    await expect(page.locator('#result')).toHaveText('You successfully clicked an alert');
+    await page.close();
+});
