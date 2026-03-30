bb50880f29e7a2f7d63f21d0a4609357ac45782f
gsk047 <gowthamsk047@gmail.com>
2026-03-30 05:33:57 +0530

    Sample Commit

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,8 +29,9 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'retain-on-failure',
-    headless: true
+    trace: 'on',
+    headless: true,
+    screenshot: 'on'
   },
 
   /* Configure projects for major browsers */
